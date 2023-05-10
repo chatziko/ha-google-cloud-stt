76e5f202cd2fdee6d572db0b9bf447c563b645e3
Kostas Chatzikokolakis <kostas@chatzi.org>
2023-05-10 18:31:53 +0300

    initial version

---
+++ README.md
@@ -0,0 +1,30 @@
+# Google Cloud Speech-To-Text for Home Assistant
+
+[![](https://img.shields.io/github/release/chatziko/ha-google-cloud-tts/all.svg?style=for-the-badge)](https://github.com/chatziko/ha-google-cloud-tts/releases)
+[![hacs_badge](https://img.shields.io/badge/HACS-Default-41BDF5.svg?style=for-the-badge)](https://github.com/hacs/integration)
+[![](https://img.shields.io/badge/MAINTAINER-%40chatziko-red?style=for-the-badge)](https://github.com/chatziko)
+[![](https://img.shields.io/badge/COMMUNITY-FORUM-success?style=for-the-badge)](https://community.home-assistant.io)
+
+
+This integration allows to use [Google Cloud Speetch-to-Text](https://cloud.google.com/speech-to-text) in Home Assistant.
+
+To use it you need to configure a Google Cloud project, following the same instructions as the
+[Google Cloud Text-to-Speach](https://www.home-assistant.io/integrations/google_cloud) integration.
+Then place the JSON file with the API key you downloaded in the `config` folder, and add the following to your `configuration.yaml`:
+
+```yaml
+tts:
+  - platform: google_cloud_stt
+    key_file: googlecloud.json
+    model: command_and_search
+```
+
+After enabling the integration, you can configure a [Voice Assistant](https://www.home-assistant.io/blog/2023/04/27/year-of-the-voice-chapter-2/#composing-voice-assistants)
+to use it by selecting `google_cloud_stt` in the "Speech-to-text" option.
+
+The supported languages are listed [here](https://cloud.google.com/speech-to-text/docs/speech-to-text-supported-languages).
+Note that V1 of Google Cloud Speetch-to-Text is used (V2 is still in preview and available in much fewer languages).
+The list of available models is avaiable [here](https://cloud.google.com/speech-to-text/docs/speech-to-text-requests#select-model). The default model
+is `command_and_search`, since it is available in most languages and should perform well in home automation tasks.
+
+
---
+++ custom_components/google_cloud_stt/stt.py
@@ -0,0 +1,278 @@
+"""Support for the cloud for speech to text service."""
+from __future__ import annotations
+
+import logging
+import os
+from collections.abc import AsyncIterable
+
+import async_timeout
+from google.cloud import speech
+import voluptuous as vol
+
+from homeassistant.components.stt import (
+    AudioBitRates,
+    AudioChannels,
+    AudioCodecs,
+    AudioFormats,
+    AudioSampleRates,
+    Provider,
+    SpeechMetadata,
+    SpeechResult,
+    SpeechResultState,
+)
+import homeassistant.helpers.config_validation as cv
+
+_LOGGER = logging.getLogger(__name__)
+
+DEFAULT_MODEL = "command_and_search"
+
+CONF_KEY_FILE = "key_file"
+CONF_MODEL = "model"
+
+SUPPORTED_LANGUAGES = [
+    "af-ZA",
+    "sq-AL",
+    "am-ET",
+    "ar-DZ",
+    "ar-BH",
+    "ar-EG",
+    "ar-IQ",
+    "ar-IL",
+    "ar-JO",
+    "ar-KW",
+    "ar-LB",
+    "ar-MA",
+    "ar-OM",
+    "ar-QA",
+    "ar-SA",
+    "ar-PS",
+    "ar-TN",
+    "ar-AE",
+    "ar-YE",
+    "hy-AM",
+    "az-AZ",
+    "eu-ES",
+    "bn-BD",
+    "bn-IN",
+    "bs-BA",
+    "bg-BG",
+    "my-MM",
+    "ca-ES",
+    "zh-CN",
+    "zh-TW",
+    "hr-HR",
+    "cs-CZ",
+    "da-DK",
+    "nl-BE",
+    "nl-NL",
+    "en-AU",
+    "en-CA",
+    "en-GH",
+    "en-HK",
+    "en-IN",
+    "en-IE",
+    "en-KE",
+    "en-NZ",
+    "en-NG",
+    "en-PK",
+    "en-PH",
+    "en-SG",
+    "en-ZA",
+    "en-TZ",
+    "en-GB",
+    "en-US",
+    "et-EE",
+    "fil-PH",
+    "fi-FI",
+    "fr-BE",
+    "fr-CA",
+    "fr-FR",
+    "fr-CH",
+    "gl-ES",
+    "ka-GE",
+    "de-AT",
+    "de-DE",
+    "de-CH",
+    "el-GR",
+    "gu-IN",
+    "iw-IL",
+    "hi-IN",
+    "hu-HU",
+    "is-IS",
+    "id-ID",
+    "it-IT",
+    "it-CH",
+    "ja-JP",
+    "jv-ID",
+    "kn-IN",
+    "kk-KZ",
+    "km-KH",
+    "ko-KR",
+    "lo-LA",
+    "lv-LV",
+    "lt-LT",
+    "mk-MK",
+    "ms-MY",
+    "ml-IN",
+    "mr-IN",
+    "mn-MN",
+    "ne-NP",
+    "no-NO",
+    "fa-IR",
+    "pl-PL",
+    "pt-BR",
+    "pt-PT",
+    "ro-RO",
+    "ru-RU",
+    "sr-RS",
+    "si-LK",
+    "sk-SK",
+    "sl-SI",
+    "es-AR",
+    "es-BO",
+    "es-CL",
+    "es-CO",
+    "es-CR",
+    "es-DO",
+    "es-EC",
+    "es-SV",
+    "es-GT",
+    "es-HN",
+    "es-MX",
+    "es-NI",
+    "es-PA",
+    "es-PY",
+    "es-PE",
+    "es-PR",
+    "es-ES",
+    "es-US",
+    "es-UY",
+    "es-VE",
+    "su-ID",
+    "sw-KE",
+    "sw-TZ",
+    "sv-SE",
+    "ta-IN",
+    "ta-MY",
+    "ta-SG",
+    "ta-LK",
+    "te-IN",
+    "th-TH",
+    "tr-TR",
+    "uk-UA",
+    "ur-IN",
+    "ur-PK",
+    "uz-UZ",
+    "vi-VN",
+    "zu-ZA",
+]
+
+SUPPORTED_MODELS = [
+    "default",
+    "command_and_search",
+    "latest_short",
+    "latest_long",
+    "phone_call",
+    "video",
+]
+
+MODEL_SCHEMA = vol.In(SUPPORTED_MODELS)
+
+PLATFORM_SCHEMA = cv.PLATFORM_SCHEMA.extend(
+    {
+        vol.Optional(CONF_KEY_FILE): cv.string,
+        vol.Optional(CONF_MODEL, default=DEFAULT_MODEL): MODEL_SCHEMA,
+    }
+)
+
+
+async def async_get_engine(hass, config, discovery_info=None):
+    """Set up Google Cloud STT component."""
+    if key_file := config.get(CONF_KEY_FILE):
+        key_file = hass.config.path(key_file)
+        if not os.path.isfile(key_file):
+            _LOGGER.error("File %s doesn't exist", key_file)
+            return None
+
+    return GoogleCloudSTTProvider(hass, key_file, config.get(CONF_MODEL))
+
+
+class GoogleCloudSTTProvider(Provider):
+    """The Google Cloud STT API provider."""
+
+    def __init__(self, hass, key_file, model) -> None:
+        """Init Google Cloud STT service."""
+        self.hass = hass
+        self.name = "Google Cloud STT"
+
+        self._model = model
+
+        # Use speech v1. For the moment speech v2 is in preview and has fewer supported languages
+        if key_file:
+            self._client = speech.SpeechClient.from_service_account_json(key_file)
+        else:
+            self._client = speech.SpeechClient()
+
+    @property
+    def supported_languages(self) -> list[str]:
+        """Return a list of supported languages."""
+        return SUPPORTED_LANGUAGES
+
+    @property
+    def supported_formats(self) -> list[AudioFormats]:
+        """Return a list of supported formats."""
+        return [AudioFormats.WAV, AudioFormats.OGG]
+
+    @property
+    def supported_codecs(self) -> list[AudioCodecs]:
+        """Return a list of supported codecs."""
+        return [AudioCodecs.PCM, AudioCodecs.OPUS]
+
+    @property
+    def supported_bit_rates(self) -> list[AudioBitRates]:
+        """Return a list of supported bitrates."""
+        return [AudioBitRates.BITRATE_16]
+
+    @property
+    def supported_sample_rates(self) -> list[AudioSampleRates]:
+        """Return a list of supported samplerates."""
+        return [AudioSampleRates.SAMPLERATE_16000]
+
+    @property
+    def supported_channels(self) -> list[AudioChannels]:
+        """Return a list of supported channels."""
+        return [AudioChannels.CHANNEL_MONO]
+
+    async def async_process_audio_stream(
+        self, metadata: SpeechMetadata, stream: AsyncIterable[bytes]
+    ) -> SpeechResult:
+        # Collect data
+        audio_data = b""
+        async for chunk in stream:
+            audio_data += chunk
+
+        # """Process an audio stream to STT service."""
+        audio = speech.RecognitionAudio(content=audio_data)
+        encoding = (
+            speech.RecognitionConfig.AudioEncoding.OGG_OPUS
+            if metadata.codec == AudioCodecs.OPUS
+            else speech.RecognitionConfig.AudioEncoding.LINEAR16
+        )
+        config = speech.RecognitionConfig(
+            language_code=metadata.language,
+            encoding=encoding,
+            sample_rate_hertz=metadata.sample_rate,
+            model=self._model,
+        )
+
+        async with async_timeout.timeout(10):
+            assert self.hass
+            response = await self.hass.async_add_executor_job(
+                lambda: self._client.recognize(config=config, audio=audio)
+            )
+            if response.results and response.results[0].alternatives:
+                return SpeechResult(
+                    response.results[0].alternatives[0].transcript,
+                    SpeechResultState.SUCCESS,
+                )
+            return SpeechResult("", SpeechResultState.ERROR)
